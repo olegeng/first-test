049ee48918b35bac926840fc41034daa1654f0a4
Oleg Pavlish <olegpa2005@gmail.com>
2022-08-07 18:37:43 +0300

    1.py - authorizate and registr

--- 1.py
+++ 1.py
@@ -1,5 +1,45 @@
-print('Hello world')
-x = input("Ввведите имя:")
-s = input('Введите пароль:')
-if x == "Oleg" and s == 'oleg2005':
-	print('Ку, Олег')
+ids = 0
+xy=0
+x = 0
+y=0
+z=0
+numb=-1
+logins = ['log123', 'Oleg Pavlish']
+Paswords = ['oleg2005', '12345678']
+vibir1=input('Регістрація/Авторизація:')
+if vibir1 == "Регістрація":
+	while xy==0:
+		new_log=input('Введіть логін:')
+		for x in logins:
+		    if x==new_log:
+		    	print('Такий логін вже є, придумайте новий.')
+		    if x!=new_log:
+		    	new_pass=input('Введіть пароль:')
+		    	perevirka_pass=input('Повторіть пароль:')
+		    	if new_pass==perevirka_pass:
+		    		logins.append(new_log)
+		    		Paswords.append(new_pass)
+		    		xy=xy+5
+		    		break
+if vibir1=='Авторизація':
+	sign=input('Введіть логін:')
+	passw=input('Введіть пароль:')
+	for x in logins:
+	    numb+=1
+	    if sign==x:
+	    	break
+	    	if passw!=Paswords[numb]:
+		        while z==0:
+			        numb=-1
+			        print('Ви провалили авторизацію, спробуйте ще раз!')
+			        sign=input('Введіть логін:')
+			        passw=input('Введіть пароль:')
+			        for x in logins:
+			            numb+=1
+			            if sign==x:
+				            if passw == Paswords[numb]:
+					            z+=5
+					            print('Ви успішно авторизувались')
+					            continue
+print(logins)
+print(Paswords)
